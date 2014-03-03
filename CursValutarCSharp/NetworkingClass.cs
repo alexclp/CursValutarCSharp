1533ac500f3df2b896d66298c838b6c63928dfca
Alexandru Clapa <alexandru.clapa@gmail.com>
2014-03-03 23:45:16 +0200

    Getting data

---
+++ CursValutarCSharp/NetworkingClass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursValutarCSharp
+{
+    class NetworkingClass
+    {
+        public static string RequestData()
+        {
+            WebRequest webRequest = WebRequest.Create("http://www.bnr.ro/nbrfxrates.xml");
+            WebResponse response = webRequest.GetResponse();
+            Console.WriteLine(((HttpWebResponse)response).StatusDescription);
+
+            Stream dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+
+            string responseFromServer = reader.ReadToEnd();
+            Console.WriteLine(responseFromServer);
+            reader.Close();
+            response.Close();
+
+            return responseFromServer;
+        }
+
+        public static void ParseData(string data)
+        {
+            
+        }
+    }
+}
--- CursValutarCSharp/Program.cs
+++ CursValutarCSharp/Program.cs
@@ -17,6 +17,7 @@ namespace CursValutarCSharp
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
         }
     }
 }
